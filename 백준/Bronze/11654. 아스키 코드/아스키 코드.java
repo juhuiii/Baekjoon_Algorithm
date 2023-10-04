14e2d43e075ea217d3079ccdaf9eb65695b6538a
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2023-10-04 13:49:23 +0900

    [Bronze V] Title: 아스키 코드, Time: 108 ms, Memory: 12912 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String string = input.next();
+        int result = string.charAt(0);
+        System.out.print(result);
+ 	}
+}
