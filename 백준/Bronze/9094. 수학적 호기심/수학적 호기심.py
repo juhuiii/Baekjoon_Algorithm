37fe5951e1f0dc9f1466c32ca2af1a3f298dd599
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2023-10-31 08:25:27 +0900

    [Bronze III] Title: 수학적 호기심, Time: 892 ms, Memory: 110416 KB -BaekjoonHub

--- 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -1,13 +1,8 @@
-k = int(input())
-for _ in range(k):
+for _ in range(int(input())):
     count = 0
     n, m = map(int, input().split()) #maybe fix
     for b in range(2, n):
-        #print()
-        #print("B is now equal to", b)
         for a in range(1, b):
-            #print("A is now equal to", a)
-            if ((b*b+a*a+m)/(a*b))%1 == 0:
-                #print("count is now added")
+            if ((b*b+a*a+m)%(a*b)) == 0:
                 count += 1
     print(count) 
