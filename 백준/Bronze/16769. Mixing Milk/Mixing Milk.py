2df736f924652cefdfb66e4e848f057d3a406ac4
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2023-11-08 07:56:44 +0900

    [Bronze II] Title: Mixing Milk, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/16769. Mixing Milk/Mixing Milk.py
@@ -0,0 +1,34 @@
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+e, f = map(int, input().split())
+if b + d > c:
+    b = b + d - c
+    d = c
+else:
+    d = b + d
+    b = 0
+
+if d + f > e:
+    d = d + f - e
+    f = e
+else:
+    f = d + f
+    d = 0
+
+if f + b > a:
+    f = f + b - a
+    b = a
+else:
+    b = f + b
+    f = 0
+
+if b + d > c:
+    b = b + d - c
+    d = c
+else:
+    d = b + d
+    b = 0
+
+print(b)
+print(d)
+print(f)
