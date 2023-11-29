bf4a1dd2b4050c8160327888c740af3463ccea0a
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2023-11-29 11:46:33 +0900

    [Bronze I] Title: 세로읽기, Time: 108 ms, Memory: 12816 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+  char[][] str = new char[5][15];
+
+  for(int i = 0; i < 5; i++) {
+    String input = in.next();
+    for (int j = 0; j < input.length(); j++) {
+      str[i][j] = input.charAt(j);
+			}
+		}
+
+		for (int i = 0; i < 15; i++) { 
+			for (int j = 0; j < 5; j++) { 
+				if (str[j][i] == '\0')
+					continue;
+				System.out.print(str[j][i]);
+
+			}
+		}
+	}
+}
