bcefa62860be0406f65b64a915a46b60e4533d9b
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2023-12-12 15:51:22 +0900

    [Bronze I] Title: 크로스워드 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2804. 크로스워드 만들기/크로스워드 만들기.py
+++ 백준/Bronze/2804. 크로스워드 만들기/크로스워드 만들기.py
@@ -1,20 +1,21 @@
-a, b = map(str, input().split())
+a, b = input().split()
+
 for i in range(len(a)) :
   if a[i] in b :
-    same = a[i]
-    a_i = i
+    ok = a[i]
+    ok_i = i
     break
 
 for j in range(len(b)) :
-  if b[j] == same :
-    b_i = j
+  if b[j] == ok:
+    bbang_i = j
     break
 
 result = ['.'] * len(a)
 for k in range(len(b)) :
-  result[a_i] = b[k]
+  result[ok_i] = b[k]
 
-  if k == b_i :
+  if k == bbang_i :
     print(a)
   else :
     print(''.join(map(str, result)))
