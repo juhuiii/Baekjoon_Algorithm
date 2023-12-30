5c9572b663cccfd686713718b3f5beae9d7ba047
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2023-12-30 13:37:40 +0900

    [Bronze III] Title: 꿍의 우주여행, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/9501. 꿍의 우주여행/꿍의 우주여행.c
+++ 백준/Bronze/9501. 꿍의 우주여행/꿍의 우주여행.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-int main()
-{
-  int t, n, d, v, f, c;
+int main(){
+  int t, n, d;
   scanf("%d", &t);
-  for(int i = 0; i < t; i++)
-    {
-      scanf("%d %d",&n, &d);
-      int count = 0;
-      for (int j = 0; j < n; j++){
-        scanf("%d %d %d", &v, &f, &c);
-        double result = (double)f / c;
-        if (result * v >= d)
-          count++;
+  int v, f, c;
+  for(int i = 0; i < t; i++){
+    scanf("%d %d", &n, &d);
+    int count = 0;
+    for(int j = 0; j < n; j++){
+      scanf("%d %d %d", &v, &f, &c);
+      double result = (double)f / c;
+      if(result * v >= d){
+        count ++;
       }
-      printf("%d\n", count);
     }
+    printf("%d\n", count);
+  }
   return 0;
 }
