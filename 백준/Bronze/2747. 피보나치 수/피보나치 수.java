73494491bc306dfde039fd1c4822820043354295
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2024-01-08 01:08:50 +0900

    [Bronze II] Title: 피보나치 수, Time: 112 ms, Memory: 13032 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt();
+    
+    int f1 = 0, f2 = 1, f3 = 1;
+
+    for (int i = 2; i <= n; i++) {
+      f3 = f1 + f2;
+      f1 = f2;
+      f2 = f3;
+    }
+      System.out.println(f3);
+  }
+}
