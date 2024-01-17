115b73f4314f0e1bcd645eb2152c667e08575d0b
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2024-01-17 12:05:15 +0900

    [Bronze II] Title: 시간복잡도를 배운 도도, Time: 532 ms, Memory: 48708 KB -BaekjoonHub

--- 백준/Bronze/24510. 시간복잡도를 배운 도도/시간복잡도를 배운 도도.java
+++ 백준/Bronze/24510. 시간복잡도를 배운 도도/시간복잡도를 배운 도도.java
@@ -1,24 +1,28 @@
-  import java.util.Scanner;
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
 
-  class Main{
-      public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      int C = sc.nextInt();
-      int result = 0;
+        int c = input.nextInt();
+        int[] count = new int[c];
 
-      for(int i = 0; i <= C; i++) {
-        int count = 0;
-        String code = sc.nextLine();
-        code = code.replaceAll("for", "!");
-        code = code.replaceAll("while", "!");
+        for(int i = 0; i < c; i++){
+            String str = input.next();
+            str = str.replaceAll("for", "~");
+            str = str.replaceAll("while", "~");
 
-        for(int j = 0; j < code.length(); j++) {
-          if(code.charAt(j) == '!')
-            count++;
+            for(int j = 0; j < str.length(); j++){
+                if(str.charAt(j) == '~')
+                    count[i]++;
+            }
         }
-        if(result < count)
-          result = count;
-      }
-      System.out.println(result);
+
+        int max = 0;
+        for(int i = 0; i < c; i++){
+            if(max < count[i]){
+                max = count[i];
+            }
+        }
+        System.out.println(max);
     }
-  }
+} 
