bac28b25098ce96a3ebe3660ad2996b1e8605ecb
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2024-01-23 08:52:10 +0900

    [Bronze III] Title: 수학적 호기심, Time: 4024 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -1,8 +1,12 @@
-for _ in range(int(input())):
+import sys
+input = sys.stdin.readline
+x = int(input())
+for i in range(x):
     count = 0
-    n, m = map(int, input().split()) #maybe fix
-    for b in range(2, n):
-        for a in range(1, b):
-            if ((b*b+a*a+m)%(a*b)) == 0:
+    n, m = map(int, input().split())
+    for j in range(1, n):
+        for k in range(j + 1, n):
+            if ((j**2+k**2+m)%(j*k)) == 0:
                 count += 1
+                #print(j, k)
     print(count) 
