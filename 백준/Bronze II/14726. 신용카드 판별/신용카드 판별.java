4b8ba8835fcfe77b08d72a61eb67d55181f0aff4
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2024-02-16 01:36:43 +0900

    [Bronze II] Title: 신용카드 판별, Time: 72 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Bronze II/14726. 신용카드 판별/신용카드 판별.java
+++ 백준/Bronze II/14726. 신용카드 판별/신용카드 판별.java
@@ -1,63 +1,36 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    static int T;
-    static Scanner sc = new Scanner(System.in);
-    static List<String> numbers = new ArrayList<>();
+public class Main{
+  public static void main(String[] agrs) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) {
-        T = sc.nextInt();
-        sc.nextLine();
-
-        for (int i = 0; i < T; i++) {
-            String temp;
-            temp = sc.nextLine();
-            numbers.add(temp);
+    int n = Integer.parseInt(br.readLine());
+    
+    for(int i = 0; i < n; i++){
+      String str = br.readLine();
+      int res = 0;
+      int count = 1;
+      for(int j = str.length() - 1; j >= 0; j--){
+        int num = Integer.parseInt(str.charAt(j) + "");
+        
+        if(count % 2 == 0){
+          num *= 2;
+          if(num >= 10) 
+            num = (num / 10 + num % 10);
         }
-
-        for (int i = 0; i < T; i++) {
-            if (isValid(numbers.get(i)))
-                System.out.println("T");
-            else
-                System.out.println("F");
-        }
+        res += num;
+        count++;
+      }
+      if(res % 10 == 0){
+        bw.write("T\n");
+      }
+      else{
+        bw.write("F\n");
+      }
     }
-
-    static boolean isValid(String number) {
-        int cnt = 1;
-
-        for (int i = number.length() - 1; i >= 0; i--) {
-            if (cnt % 2 != 0) {
-                // 추후 모든 자리의 수를 더하기 위해 숫자로 저장
-                char[] tempNumber = number.toCharArray();
-                tempNumber[i] -= '0';
-                number = String.valueOf(tempNumber);
-
-                cnt++;
-                continue;
-            }
-
-            int twiceNum = (number.charAt(i) - '0') * 2;
-            if (twiceNum >= 10) {
-                // 2배로 만든 수는 무조건 10 이상 18 이하이므로 1 + (수 - 10)으로 계산
-                twiceNum = 1 + (twiceNum - 10);
-            }
-
-            // 추후 모든 자리의 수를 더하기 위해 숫자로 저장
-            char[] tempNumber = number.toCharArray();
-            tempNumber[i] = (char)twiceNum;
-            number = String.valueOf(tempNumber);
-
-            cnt++;
-        }
-
-        int k = 0;
-
-        for (int i = 0; i < number.length(); i++)
-            k += number.charAt(i);
-
-        return (k % 10 == 0) ? true : false;
-    }
+    bw.flush();
+    bw.close();
+  }
 }
