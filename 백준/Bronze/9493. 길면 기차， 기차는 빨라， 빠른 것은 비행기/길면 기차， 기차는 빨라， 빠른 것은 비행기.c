4afc8954a1bd0db1489165c8989d3c810055339f
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2024-03-09 13:50:41 +0900

    [Bronze III] Title: 길면 기차, 기차는 빨라, 빠른 것은 비행기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9493. 길면 기차， 기차는 빨라， 빠른 것은 비행기/길면 기차， 기차는 빨라， 빠른 것은 비행기.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    double m, a, b;
+    while (1) {
+        scanf("%lf %lf %lf", &m, &a, &b);
+        if (m == 0 && a == 0 && b == 0)
+        break;
+        
+        int time = round(m * 3600 * (1 / a - 1 / b)) ;
+        
+        int hour = time / 3600;
+        int minute = (time % 3600) / 60;
+        int sec = time% 60;
+        printf("%d:%02d:%02d\n", hour, minute, sec);
+    }
+    return 0;
+}
