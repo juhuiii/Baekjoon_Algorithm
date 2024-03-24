828389455c6ef2bbc34faa5e9d8747ce96b6762a
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2024-03-24 19:29:38 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
@@ -1,2 +1,11 @@
-for i in range(int(input())):
-    print("Case #%d:"%(i+1), " ".join(list(reversed(input().split(" ")))))
+a = int(input())
+c = []
+for i in range(a) :
+  b = list(input().split())
+  c.append(b)
+for i in range(a) :
+  c[i].reverse()
+
+for i in range(a) :
+  print("Case #%d:"%(i+1), end = ' ')
+  print(*c[i])
