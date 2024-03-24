4a8b4d0a456791352235ebfe403b9b9b1b387ae8
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2024-03-24 17:21:04 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -1,16 +1,15 @@
-a1 = int(input())
-a2 = int(input())
-a3 = int(input())
-if a1 + a2 + a3 == 180:
-    if a1 == a2 == a3:
-        print("Equilateral")
-    elif a1 == a2 and a1 != a3:
-        print("Isosceles")
-    elif a2 == a3 and a2 != a1:
-        print("Isosceles")
-    elif a1 == a3 and a1 != a2:
-        print("Isosceles")
-    elif a1 != a2 and a1 != a3 and a2 != a3:
-        print("Scalene")
-else:
-    print("Error")
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a == b == c == 60 :
+  print("Equilateral")
+
+elif (a + b + c) == 180 :
+  if a == b or a == c or b == c :
+    print("Isosceles")
+  else :
+    print("Scalene")
+
+else :
+  print("Error")
