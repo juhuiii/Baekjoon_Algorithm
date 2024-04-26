53fdeed7c34f16ebb5b749eb823fc1222b84f4c4
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2024-04-27 00:41:42 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int cnt = 0;
+    for(int i = 1; i <= n; i++){
+        if(n % i == 0){
+            cnt++;
+            if(cnt == k){
+                printf("%d", i);
+                break;
+            }
+        }
+    }
+    if(cnt != k){
+        printf("0");
+    }
+    return 0;
+}
