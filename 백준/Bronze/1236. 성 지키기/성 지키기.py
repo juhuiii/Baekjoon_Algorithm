bf2ccc4a32ccacb40d8375df07b248d8f7058033
JuHui Lee <49511360+juhuiii@users.noreply.github.com>
2024-04-30 16:09:13 +0900

    [Bronze I] Title: 성 지키기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1236. 성 지키기/성 지키기.py
+++ 백준/Bronze/1236. 성 지키기/성 지키기.py
@@ -1,17 +1,16 @@
-n, m = map(int,input().split())
-board = []
+n, m = map(int, input().split())
+c = []
 
-for _ in range(n):
-    board.append(input())
+for i in range(n) :
+  c.append(input())
 
 a, b = 0, 0
 
-for i in range(n):
-    if "X" not in board[i]:
-        a += 1
+for i in range(n) :
+  if "X" not in c[i] :
+    a += 1
+for j in range(m) :
+  if "X" not in [c[i][j] for i in range(n)] :
+    b += 1
 
-for j in range(m):
-    if "X" not in [board[i][j] for i in range(n)]:
-        b += 1
-
-print(max(a ,b))
+print(max(a, b))
